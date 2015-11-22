33e33d96fde5298192490d06fac36d6209af2253
Guillaume Libersat <glibersat@sigill.org>
2015-11-22 11:56:21 +0100

    remove duplicate logMessage() from brewpi.py (in favor of BrewPiUtil)

--- brewpi.py
+++ brewpi.py
@@ -72,6 +72,7 @@ import brewpiVersion
 import pinList
 import expandLogMessage
 import BrewPiProcess
+from BrewPiUtil import logMessage
 
 
 # Settings will be read from controller, initialize with same defaults as controller
@@ -96,9 +97,6 @@ cv = dict(beerDiff=0.000, diffIntegral=0.000, beerSlope=0.000, p=0.000, i=0.000,
 deviceList = dict(listState="", installed=[], available=[])
 
 lcdText = ['Script starting up', ' ', ' ', ' ']
-
-def logMessage(message):
-    printStdErr(time.strftime("%b %d %Y %H:%M:%S   ") + message)
 
 # Read in command line arguments
 try:
@@ -851,4 +849,3 @@ if ser:
 if conn:
     conn.shutdown(socket.SHUT_RDWR)  # close socket
     conn.close()
-
